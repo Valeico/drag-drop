88c3cfa4fe86d7d3d41cfe85e686b7f731266fe8
Francisco Medero <valeico.dev@gmail.com>
2023-05-29 11:00:41 -0700

    index

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/drag-and-drop-example/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/drag-drop/'
 })
